f1f042688b841e9f051e822ec7dd4ae741b94ed1
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-08 11:39:26 +0530

    [kunal]commiting changes for calculate length of line program

---
+++ LineComparisons/LineComparisons/LineComparison.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisons
+{
+   public class LineComparison
+    {
+        public void LengthofLine()
+        {
+            Console.WriteLine("Enter value for x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());       
+            Console.WriteLine("Enter value for x2: ");              
+            int x2 = Convert.ToInt32(Console.ReadLine());         
+            Console.WriteLine("Enter value for y1: ");                
+            int y1 = Convert.ToInt32(Console.ReadLine());           
+            Console.WriteLine("Enter value for y2: ");                
+            int y2 = Convert.ToInt32(Console.ReadLine());  
+            double LengthofLine = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2); 
+            Console.WriteLine("Length of line is:" + LengthofLine);  
+        }
+   }
+}
---
+++ LineComparisons/LineComparisons/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LineComparisons;
+Console.WriteLine("Hello, World!");
+//UC-1
+LineComparison length = new LineComparison();
+length.LengthofLine();
