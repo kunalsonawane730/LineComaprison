87975e195550c09866c24001ea59cfb850b6145c
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-08 11:51:07 +0530

    [kunal]commiting changes for checking equality of lines  program

--- LineComparisons/LineComparisons/LineComparison.cs
+++ LineComparisons/LineComparisons/LineComparison.cs
@@ -21,5 +21,41 @@ namespace LineComparisons
             double LengthofLine = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2); 
             Console.WriteLine("Length of line is:" + LengthofLine);  
         }
+        //UC-2
+        public void EqualityofLines()
+        {
+            Console.WriteLine("Enter value for x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter value for j1: ");
+            int j1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for j2: ");
+            int j2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for k1: ");
+            int k1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for k2: ");
+            int k2 = Convert.ToInt32(Console.ReadLine());
+
+            double LengthofLine1 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            double LengthofLine2 = Math.Sqrt((j2 - j1) ^ 2 + (k2 - k1) ^ 2);
+            Console.WriteLine("Length of Line1 : " + LengthofLine1);
+            Console.WriteLine("Length of Line2 : " + LengthofLine2);
+
+            if (LengthofLine1.Equals(LengthofLine2))
+            {
+                Console.WriteLine("Two lines are equal ");
+            }
+            else
+            {
+                Console.WriteLine("Two lines are Not equal");
+            }
+        }
    }
 }
